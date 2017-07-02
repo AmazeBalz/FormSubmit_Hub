f754d7d08ee9df7f121fcc9d473e69b9c756f022
jack <jackhickey@gmail.com>
2017-07-02 19:07:12 +0100

    it works

--- index.js
+++ index.js
@@ -1,47 +1,48 @@
 const express = require('express');
 const app = express();
 const request = require('request-promise');
-const querystring = require('querystring');
 const path = require('path');
+const bodyParser = require('body-parser')
 
 const HUB_ENDPOINT = 'https://forms.hubspot.com/uploads/form/v2/3452256/205a6073-77cf-4f19-9132-2dd39d259ccd';
 
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
 app.post('/api/form', (req, res) => {
 
+  let postData = {
+    email: req.body.email,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    hs_context: {
+      //"hutk": req.cookies.hubspotutk,
+      ipAddress: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+      pageUrl: "http://www.example.com/form-page",
+      pageName: "Example Title"
+    }};
 
-  return res.status(200).send();
+  let options = {
+    uri: HUB_ENDPOINT,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': postData.length
+    },
+    form: postData
+  }
 
-// let postData = querystring.stringify({
-//     'email': req.body.email,
-//     'firstname': req.body.firstname,
-//     'lastname': req.body.lastname,
-//     'hs_context': JSON.stringify({
-//         "hutk": req.cookies.hubspotutk,
-//         "ipAddress": req.headers['x-forwarded-for'] || req.connection.remoteAddress,
-//         "pageUrl": "http://www.example.com/form-page",
-//         "pageName": "Example Title"
-//     })
-// });
+  request(options)
+  .then((response) => {
+    return res.send();
+  })
+  .catch((error) => res.status(500).send(error));
 
-// let options = {
-// 	hostname: 'forms.hubspot.com',
-// 	path: '/uploads/form/v2/3452256/205a6073-77cf-4f19-9132-2dd39d259ccd',
-// 	method: 'POST',
-// 	headers: {
-// 		'Content-Type': 'application/x-www-form-urlencoded',
-// 		'Content-Length': postData.length
-// 	},
-//   body: postData
-// }
-
-//   return request(options)
-//   .catch((error) => console.error);
 })
 
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => {
   console.log('Server has started...')
